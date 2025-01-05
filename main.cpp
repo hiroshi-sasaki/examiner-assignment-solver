bdfb6af6f28807a87e729cf594f17e0605946347
ebi-fly13 <yano.h.ag@m.titech.ac.jp>
2025-01-05 13:13:51 +0900

    update

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 
 #include "concept_presentation/concept_presentation.cpp"
 #include "research_project/research_project.cpp"
+#include "memo/io.hpp"
+#include "masters_presentation/solver.hpp"
 #include "util/io_util.cpp"
 
 int main(int argc, char *argv[]) {
@@ -18,6 +20,12 @@ int main(int argc, char *argv[]) {
         std::string filename = argv[2];
         int type = std::stoi(argv[3]);
         research_project::run(filename, type);
+    } else if(event == "masters_presentation") {
+        assert(argc == 4);
+        std::string professor_filename = argv[2];
+        std::string student_filename = argv[3];
+        examiner_assignment::examiner_assignment_solver solver(professor_filename, student_filename);
+        solver.run();
     } else {
         assert(0);
     }
